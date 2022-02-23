207c01a56b09170157cf7dfc63660971913f97d1
ameyd96 <erameydeshpande@gmail.com>
2022-02-23 00:57:41 -0800

    Spring security with JPA and MYSQL project is created

---
+++ SpringSecurit-jpa-mysql/src/main/java/com/controller/SpringSecuritJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritJpaMysqlApplication.class, args);
+	}
+
+}
