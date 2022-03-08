a84c70b156cbee68104965b994d9cd37d99e6acb
ameyd96 <erameydeshpande@gmail.com>
2022-03-08 01:33:29 -0800

    Adding simple logger

--- spring/src/main/java/com/app/controller/DepartmentController.java
+++ spring/src/main/java/com/app/controller/DepartmentController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +20,18 @@ import com.app.entity.Department;
 import com.app.service.DepartmentService;
 
 
+
 @RestController
 public class DepartmentController {
 	
 	@Autowired(required=true)
 	private DepartmentService departmentService;
 	
+	private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+	
 	@PostMapping("/departments")
 	public Department saveDepartments(@Valid @RequestBody Department department) {
+		LOGGER.info("Inside saveDepartment of DepartmentController");
 		return departmentService.saveDepartments(department);
 		 
 		
@@ -34,7 +40,7 @@ public class DepartmentController {
 	
 	@GetMapping("/departments")
 	public List<Department> fetchDepartmentList(){
-		
+		LOGGER.info("Inside fetchDepartmentList of DepartmentController");
 		return departmentService.fetchDepartmentList();
 		
 	}
