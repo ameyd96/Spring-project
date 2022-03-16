c3a91c5de9124b2c0e2b050ef0cbb599b8f54ab3
ameyd96 <erameydeshpande@gmail.com>
2022-03-16 03:01:43 -0700

    React Router 6 has been added

--- SpringBoot-ReactJS EmployeeManagement/EmployeeSystemUI/app/src/App.js
+++ SpringBoot-ReactJS EmployeeManagement/EmployeeSystemUI/app/src/App.js
@@ -1,12 +1,24 @@
+import {BrowserRouter, Route,Routes} from 'react-router-dom'
 import "./App.css";
 import AddEmployee from "./components/AddEmployee";
+import EmployeeList from './components/EmployeeList';
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <AddEmployee />
+    <BrowserRouter>
+    <NavBar />
+    <Routes>
+    <Route index element={<EmployeeList />}></Route>
+      <Route path='/' element={<EmployeeList />}> </Route>
+
+      <Route path='/addEmployee' element={<AddEmployee />}></Route>
+    </Routes>
+      {/* <AddEmployee />
+      <EmployeeList /> */}
+      </BrowserRouter>
+      
     </>
   );
 }
