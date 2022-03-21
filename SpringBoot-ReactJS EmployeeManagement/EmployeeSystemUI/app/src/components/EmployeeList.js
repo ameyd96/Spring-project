183b8a77b05259e0bfb74ffbaf54f5942c73489f
ameyd96 <erameydeshpande@gmail.com>
2022-03-21 00:16:20 -0700

    get employee from db

--- SpringBoot-ReactJS EmployeeManagement/EmployeeSystemUI/app/src/components/EmployeeList.js
+++ SpringBoot-ReactJS EmployeeManagement/EmployeeSystemUI/app/src/components/EmployeeList.js
@@ -1,48 +1,90 @@
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import EmployeeService from "../services/EmployeeService";
 
 const EmployeeList = () => {
-
+  const [employee, setEmployee] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await EmployeeService.getEmployee();
+      setEmployee(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-   <div className='container mx-auto my-8'>
-    <div className='h-12'> 
-      <button className='rounded bg-slate-600 text-white px-6 py-2'
-      onClick={() =>navigate("/addEmployee")}
-      >Add Employee</button>
+    <div className="container mx-auto my-8">
+      <div className="h-12">
+        <button
+          className="rounded bg-slate-600 text-white px-6 py-2"
+          onClick={() => navigate("/addEmployee")}
+        >
+          Add Employee
+        </button>
       </div>
-      <div className='flex shadow border-b'>
-        <table className='min-w-full'>
-          <thead className='bg-gray-100'>
+      <div className="flex shadow border-b">
+        <table className="min-w-full">
+          <thead className="bg-gray-100">
             <tr>
-              <th className='text-left font-medium uppercase tracking-wider px-6 py-3'>First Name</th>
-              <th className='text-left font-medium uppercase tracking-wider px-6 py-3'>Last Name</th>
-              <th className='text-left font-medium uppercase tracking-wider px-6 py-3'>Email</th>
-              <th className='text-right font-medium uppercase tracking-wider px-6 py-3'>Actions</th>
+              <th className="text-left font-medium uppercase tracking-wider px-6 py-3">
+                First Name
+              </th>
+              <th className="text-left font-medium uppercase tracking-wider px-6 py-3">
+                Last Name
+              </th>
+              <th className="text-left font-medium uppercase tracking-wider px-6 py-3">
+                Email
+              </th>
+              <th className="text-right font-medium uppercase tracking-wider px-6 py-3">
+                Actions
+              </th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td className='text-left px-6 py-4 whitespace-nowrap'>
-                <div className='text-sm text-gray-500'>aa</div>
-              </td>
-              <td className='text-left px-6 py-4 whitespace-nowrap'>
-                <div className='text-sm text-gray-500'>dd</div>
-              </td>
-              <td className='text-left px-6 py-4 whitespace-nowrap'>
-                <div className='text-sm text-gray-500'>aa@mail.com</div>
-              </td>
-              <td className='text-right px-6 py-4 whitespace-nowrap font-medium text-sm'>
-                <a href='#' className='text-indigo-500 hover:text-indigo-800 px-4'>Edit</a>
-                <a href='#' className='text-indigo-500 hover:text-indigo-800 '>Delete</a>
-              </td>
-            </tr>
-          </tbody>
+          {!loading && (
+            <tbody>
+              {employee.map((emp) =>(
+              <tr key={emp.id}>
+                <td className="text-left px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-500">{emp.firstName}</div>
+                </td>
+                <td className="text-left px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-500">{emp.lastName}</div>
+                </td>
+                <td className="text-left px-6 py-4 whitespace-nowrap">
+                  <div className="text-sm text-gray-500">{emp.email}</div>
+                </td>
+                <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+                  <a
+                    href="#"
+                    className="text-indigo-500 hover:text-indigo-800 px-4"
+                  >
+                    Edit
+                  </a>
+                  <a
+                    href="#"
+                    className="text-indigo-500 hover:text-indigo-800 "
+                  >
+                    Delete
+                  </a>
+                </td>
+              </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
--- SpringBoot-ReactJS EmployeeManagement/EmployeeSystemUI/app/src/services/EmployeeService.js
+++ SpringBoot-ReactJS EmployeeManagement/EmployeeSystemUI/app/src/services/EmployeeService.js
@@ -7,6 +7,10 @@ class EmployeeService {
         return axios.post(EMPLOYEE_API_BASE_URL,employee);
     }
 
+    getEmployee() {
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
 }
 
 export default new EmployeeService();
